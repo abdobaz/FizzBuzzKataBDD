84b98d935843011587d020abfa2eb704273816aa
Abderrahim <abderrahim.m.elbaz@gmail.com>
2018-09-04 15:15:00 +0200

    Code refactoring

--- src/main/java/fizzbuzzkata/FizzBuzz.java
+++ src/main/java/fizzbuzzkata/FizzBuzz.java
@@ -6,12 +6,13 @@ public class FizzBuzz {
 	private final static String NEW_LINE = "\n";
 	private final static String FIZZ = "Fizz";
 	private final static String BUZZ = "Buzz";
+	private final static String FIZZ_BUZZ = "FizzBuzz";
 
 	public String run(int firstNumber, int secondNumber) {
 		String result = STRING_EMPTY;
 		for (int number = firstNumber; number <= secondNumber; number++) {
-			if ((number % 3 ==0) && (number % 5 ==0)) {
-				result += "FizzBuzz";
+			if ((number % 15 ==0)) {
+				result += FIZZ_BUZZ;
 			}
 			else if (number % 3 == 0) {
 				result += FIZZ;
