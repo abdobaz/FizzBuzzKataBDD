37c835eade3c4360e0f8f1f01de96d582c09bd42
Abderrahim <abderrahim.m.elbaz@gmail.com>
2018-09-04 15:01:52 +0200

    Code refactoring

--- src/main/java/fizzbuzzkata/FizzBuzz.java
+++ src/main/java/fizzbuzzkata/FizzBuzz.java
@@ -2,18 +2,23 @@ package fizzbuzzkata;
 
 public class FizzBuzz {
 
+	private final static String STRING_EMPTY = "";
+	private final static String NEW_LINE = "\n";
+	private final static String FIZZ = "Fizz";
+	private final static String BUZZ = "Buzz";
+
 	public String run(int firstNumber, int secondNumber) {
-		String result="";
-		for (int i = firstNumber; i <= secondNumber; i++) {
-			if(i%3==0) {
-				result+="Fizz";
-			}else if(i%5==0) {
-				result+="Buzz";
-			}else {
-				result+=String.valueOf(i);
+		String result = STRING_EMPTY;
+		for (int number = firstNumber; number <= secondNumber; number++) {
+			if (number % 3 == 0) {
+				result += FIZZ;
+			} else if (number % 5 == 0) {
+				result += BUZZ;
+			} else {
+				result += String.valueOf(number);
 			}
-			if(i!=secondNumber) {
-				result+="\n";
+			if (number != secondNumber) {
+				result += NEW_LINE;
 			}
 		}
 		return result;
