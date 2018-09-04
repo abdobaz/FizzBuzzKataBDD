02572557a66ee3b27ccf826bc2517de0a18d38fa
Abderrahim <abderrahim.m.elbaz@gmail.com>
2018-09-04 14:55:53 +0200

    make second & third senarios pass

--- src/main/java/fizzbuzzkata/FizzBuzz.java
+++ src/main/java/fizzbuzzkata/FizzBuzz.java
@@ -3,7 +3,20 @@ package fizzbuzzkata;
 public class FizzBuzz {
 
 	public String run(int firstNumber, int secondNumber) {
-		return "1\n2";
+		String result="";
+		for (int i = firstNumber; i <= secondNumber; i++) {
+			if(i%3==0) {
+				result+="Fizz";
+			}else if(i%5==0) {
+				result+="Buzz";
+			}else {
+				result+=String.valueOf(i);
+			}
+			if(i!=secondNumber) {
+				result+="\n";
+			}
+		}
+		return result;
 	}
 
 }
